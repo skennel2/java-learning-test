93689b35ab062462c95a4a524d37a655c455f1e2
나윤수 <skennel2@gmail.com>
2018-03-26 17:57:18 +0900

    first commit

---
+++ src/main/java/org/almansa/app/JavaPractice/App.java
@@ -0,0 +1,13 @@
+package org.almansa.app.JavaPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
