1e7379b660b81b9e81fab70d9d6ef61a84c3fd3c
skennel2 <skennel2@gmail.com>
2018-05-23 06:35:55 -0700

    박싱 언박싱

---
+++ src/test/java/org/almansa/app/JavaPractice/collection/CollectionBasicTest.java
@@ -0,0 +1,20 @@
+package org.almansa.app.JavaPractice.collection;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+public class CollectionBasicTest {
+    @Test
+    public void 컬렉션의박싱과언박싱() {
+        Vector vector = new Vector();
+        // 벡터에 기본 타입을 아이템으로 추가한다. 
+        // 원칙적으로 자바의 모든 컬렉션은 Object만을 item으로 가진다.
+        // 기본타입은 Object형이 아니다. 
+        // 자바 1.5 이하에서는 기본형을 아이템으로 추가하려면 Wrapper 클래스로 박싱해서 사용해야했다.
+        // 1.5 이후부터는 기본타입을  인자로 사용해는 것을 허용하며 원리는 자동박싱이다. 
+        vector.add(1); // 자동 박싱
+        
+        int i = (int) vector.get(0); // 자동 언박싱 
+    }
+}
--- src/test/java/org/almansa/app/JavaPractice/string/StringTest.java
+++ src/test/java/org/almansa/app/JavaPractice/string/StringTest.java
@@ -1,21 +1,39 @@
 package org.almansa.app.JavaPractice.string;
 
-import java.util.StringTokenizer;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class StringTest {
-    
+    /**
+     * 스트링 리터럴과 인스턴스의 비교 
+     */
     @Test
-    public void testStringTokenizer() {
-        // StringTokenizer implements Enumeration<Object>
-        // 문자열을 토큰으로 분리하는데 사용하는 클래스 
-        // 레거시 클래스로서 새로운 코드에서 사용은 자제하고 String의 split을 사용하라
+    public void testStringliteralAndInstance() {
+        String strInstance = new String("1234");
+        String strLiteral1 = "1234";
+        String strLiteral2 = "1234";
         
-        StringTokenizer stringTokenizer = new StringTokenizer("aaa|bbb|ccc|", "|");
-        while(stringTokenizer.hasMoreTokens()) {
-            System.out.println(stringTokenizer.nextToken());
-        }       
+        assertEquals(false, strInstance == strLiteral1);
+        assertEquals(true, strInstance.equals(strLiteral1));
+        // 스프링 리터럴은 JVM 내의 스트링 리터럴 테이블 내의 인스턴스를 공유한다. 
+        // 그러므로 아래 테스트는 통과한다. 
+        assertEquals(true, strLiteral2.equals(strLiteral1)); 
     }
     
+    @Test
+    public void testStringIsImmutable() {
+        String string = "1234";
+        String concat = string.concat("5678");
+        
+        // String의 concat은 실제 인스턴스를 변경하는 것이 아닌 새로운 인스턴스를 반환한다.
+        // 이것은 String의 모든 메소드들이 공유하는 메커니즘으로 이러한 특성을 Immutable이라고한다.
+        assertEquals(concat, "12345678");
+        
+        String string2 = "12345";
+        String concat2 = string2.concat("67890");
+        
+        // concat으로 리턴되는 스트링은 리터럴이 아니다. 그냥 새로운 인스턴스를 생성해서 리턴하는 것이다. 
+        assertEquals(false, concat2 == "1234567890");
+    }
 }
