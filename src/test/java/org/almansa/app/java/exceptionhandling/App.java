371aef203234f6fa724e13f202ff601f0b480bdd
나윤수 <skennel2@gmail.com>
2018-06-18 10:35:24 +0900

    Exception Best Practice

--- src/test/java/org/almansa/app/java/exceptionhandling/App.java
+++ src/test/java/org/almansa/app/java/exceptionhandling/App.java
@@ -2,9 +2,38 @@ package org.almansa.app.java.exceptionhandling;
 
 public class App {
 
-    public static void main(String[] args) {
-        ExceptionService service = new ExceptionService();
-        service.raiseExceptionAndThrow();
+    public static void main(String[] args) throws Exception {
+        try {
+            exceptionThrow();
+        } catch (Exception e) {
+            System.out.println("exceptionThrow Exception catch");
+        }
+        
+        System.out.println("next");
+        
+        try {
+            trycatchExceptionThrowWrap();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("next");
     }
-
+    
+    public static void exceptionThrow() throws Exception {
+        throw new Exception();
+    }
+    
+    public static void trycatchExceptionThrow() {
+        new Long("123e");
+    }
+    
+    
+    public static void trycatchExceptionThrowWrap() {
+        trycatchExceptionThrow();
+    }
+    
+    public static void runtimeExceptionThrow() throws RuntimeException {
+        throw new RuntimeException();
+    }
 }
