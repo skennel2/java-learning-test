e8273c039ca9fbe00dffe7cff49b7cd92d46f177
나윤수 <skennel2@gmail.com>
2018-08-21 10:01:05 +0900

    Iterable

---
+++ src/test/java/org/almansa/app/java/collection/iterable/IterableTest.java
@@ -0,0 +1,27 @@
+package org.almansa.app.java.collection.iterable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+public class IterableTest {
+    @Test
+    public void 요소를_차례대로_반환하는_Iterable() {
+    	List<String> list = Arrays.asList("apple", "banana", "grape", "pair", "cherry");
+    	
+    	// Iterable은 자바의 컬렉션들의 root 인터페이스이다.
+    	Iterable<String> iterable = list;
+
+    	// Iterator를 반환하는 iterator()메소드를 노출한다.
+    	Iterator<String> iterator = iterable.iterator();
+    	String item1 = iterator.next();    	
+    	assertEquals("apple", item1);
+    	
+    	String item2 = iterator.next();
+    	assertEquals("banana", item2);
+    }
+}
