b5e8ea3ed4691e3674eaa1ba2f20febd3f2489f8
skennel2 <skennel2@gmail.com>
2018-08-27 23:45:04 +0900

    람다의 외부변수 특징

--- src/test/java/org/almansa/app/java/lambda/UsingOuterVariableTest.java
+++ src/test/java/org/almansa/app/java/lambda/UsingOuterVariableTest.java
@@ -21,12 +21,12 @@ public class UsingOuterVariableTest {
         boolean isEqual = isIgnoreCaseEquals.test("banana");
         assertEquals(true, isEqual);
     }
-    
+
     @Test
     public void 익명_인스턴스의_경우에도_마찬가지() {
         String fruit = "banana";
         Predicate<String> isIgnoreCaseEquals = new Predicate<String>() {
-            
+
             @Override
             public boolean test(String t) {
                 return t.equals(fruit);
@@ -36,9 +36,44 @@ public class UsingOuterVariableTest {
         // 아래 구문의 주석을 풀면 컴파일에러
         // 람다의 바디에서 사용되는 외부 변수는 변경될 수 없다.
 
-        //fruit = "apple";
+        // fruit = "apple";
 
         boolean isEqual = isIgnoreCaseEquals.test("banana");
         assertEquals(true, isEqual);
+    }
+
+    @Test
+    public void 인스턴스_구현체_내부의_상태값은_관리하지_않는다() {
+        PredicateImpl predicate = new PredicateImpl();
+        Predicate<String> fruitPredicate = predicate;
+
+        predicate.setValue("banana");
+
+        boolean isBanana = fruitPredicate.test("banana");
+        assertEquals(true, isBanana);
+        
+        predicate.setValue("apple");
+        
+        boolean isApple = fruitPredicate.test("banana");
+        assertEquals(false, isApple);
+    }
+
+    public class PredicateImpl implements Predicate<String> {
+
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean test(String t) {
+            return t.equals(value);
+        }
+
     }
 }
