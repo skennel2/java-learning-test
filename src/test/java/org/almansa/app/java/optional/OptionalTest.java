92fa1e7b3e37aaf9d103ac6efe0246b06a4dbdfe
skennel2 <skennel2@gmail.com>
2018-09-21 14:12:30 +0900

    Optional

---
+++ src/test/java/org/almansa/app/java/optional/OptionalTest.java
@@ -0,0 +1,32 @@
+package org.almansa.app.java.optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class OptionalTest {
+	
+	@Test
+	public void optional생성하기_ofNullable() {
+		Optional<String> opt = Optional.ofNullable("abc");
+		
+		assertEquals("abc", opt.get());
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void optional생성하기_ofNullable_null_값을_주었을때() {
+		Optional<String> opt = Optional.ofNullable(null); //ok
+		
+		String value = opt.get(); // NoSuchElementException
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void optional생성하기_of_null_값을_주었을때() {
+		Optional<String> opt = Optional.of(null); // NullPointerException
+		
+		String value = opt.get(); 
+	}
+}
