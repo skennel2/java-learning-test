e1f4e037b25ef2dcb919c8ac5073fb78e3bf2470
skennel2 <skennel2@gmail.com>
2019-01-22 13:08:14 +0900

    파일명, 주석 정리

--- src/test/java/org/almansa/app/java/date/DateAndLocalDateTimeTest.java
+++ src/test/java/org/almansa/app/java/date/DateAndLocalDateTimeTest.java
@@ -7,12 +7,12 @@ import java.util.Date;
 
 import org.junit.Test;
 
-public class DateTest {
+public class DateAndLocalDateTimeTest {
 	@Test
 	public void Date클래스는_가변클래스() {
 		DateWrapper wrapper = new DateWrapper(new Date(2019, 1, 22));
 		Date date = wrapper.getDate();
-		date.setYear(2018);
+		date.setYear(2018); // 데이터를 변경할 수 있는 setter를 노출한다.
 
 		// 결론적으로는 이게 Date 클래스의 한계
 		assertEquals(2018, wrapper.getDate().getYear());
@@ -22,7 +22,7 @@ public class DateTest {
 	public void LocalDateTime은_불변클래스() {
 		LocalDateWrapper localDateWrapper = new LocalDateWrapper(LocalDateTime.of(2019, 1, 22, 0, 0));
 		
-		// 여느 Immutable 클래스들 처럼 새로운 LocalDateTime를 반환할 뿐 데이터를 변경하지는 않는다.
+		// 여느 Immutable 클래스들 처럼 setter를 노출하지 않고 새로운 LocalDateTime를 반환한다.
 		LocalDateTime localDateTime = localDateWrapper.getDate().plusMonths(1);
 		
 		assertEquals(1, localDateWrapper.getDate().getMonth().getValue());
