b723e722e386cfd674fc82a1838a13854d066584
skennel2 <skennel2@gmail.com>
2019-01-25 00:28:11 +0900

    WeakHashMap

--- src/test/java/org/almansa/app/java/collection/map/WeakHashMapTest.java
+++ src/test/java/org/almansa/app/java/collection/map/WeakHashMapTest.java
@@ -1,12 +1,25 @@
 package org.almansa.app.java.collection.map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.ref.WeakReference;
 import java.util.WeakHashMap;
+
+import org.junit.Test;
 
 public class WeakHashMapTest {
 
-	public void test() {
-		WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+	@Test
+	public void weakHashMap() {
+		WeakHashMap<WeakReference<String>, String> weakHashMap = new WeakHashMap<>();
 		
+		String key1S = new String("key1");
+		WeakReference<String> key1 = new WeakReference<String>(key1S);
 		
+		weakHashMap.put(key1, "entry1");
+		
+		key1 = null;
+		
+		assertEquals(false, weakHashMap.containsKey(key1));
 	}
 }
