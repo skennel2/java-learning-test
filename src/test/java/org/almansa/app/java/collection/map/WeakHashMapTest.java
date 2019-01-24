6e011d49abfb988d031be9871669c743780a507b
skennel2 <skennel2@gmail.com>
2019-01-24 23:47:26 +0900

    WeakHashMap

---
+++ src/test/java/org/almansa/app/java/collection/map/WeakHashMapTest.java
@@ -0,0 +1,12 @@
+package org.almansa.app.java.collection.map;
+
+import java.util.WeakHashMap;
+
+public class WeakHashMapTest {
+
+	public void test() {
+		WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+		
+		
+	}
+}
