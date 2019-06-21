b32c2d3912f380e9837c55f93d18b48a54cb73ca
skennel2 <skennel2@gmail.com>
2019-06-21 16:11:27 +0900

    LocalDate

--- src/test/java/org/almansa/app/java/date/TimePackageClassTest.java
+++ src/test/java/org/almansa/app/java/date/TimePackageClassTest.java
@@ -1,8 +1,13 @@
 package org.almansa.app.java.date;
 
+import static org.junit.Assert.assertEquals;
+
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.TemporalAdjusters;
 
 import org.junit.Test;
 
@@ -18,4 +23,42 @@ public class TimePackageClassTest {
 		// LocalDateTime은 연 월 일 시 분 초 나노초까지 표현한다.
 		LocalDateTime localDateTime = LocalDateTime.of(2019, 1, 22, 12, 0, 59);			
 	}
+	
+	@Test
+	public void 날짜연산하기() {
+		LocalDate localDate = LocalDate.of(2019, 1, 1);
+		localDate = localDate.minus(Period.ofYears(1));
+		
+		assertEquals(LocalDate.of(2018, 1, 1), localDate);
+		
+		localDate = LocalDate.of(2019, 1, 1);
+		localDate = localDate.minus(Period.ofDays(1));
+		
+		assertEquals(LocalDate.of(2018, 12, 31), localDate);
+	}
+	
+	@Test
+	public void 날짜비교하기() {
+		LocalDate localDate = LocalDate.of(2019, 1, 1);
+		
+		boolean isBefore = localDate.isBefore(LocalDate.of(2019, 1, 2));
+		
+		assertEquals(true, isBefore);		
+	}
+	
+	@Test
+	public void 날짜조정하기() {
+		LocalDate localDate = LocalDate.of(2019, 2, 15);		
+		localDate = localDate.with(TemporalAdjusters.lastDayOfMonth());		
+		assertEquals(LocalDate.of(2019, 2, 28), localDate);
+		
+		localDate = LocalDate.of(2019, 3, 31).withMonth(2);
+		assertEquals(LocalDate.of(2019,  2, 28), localDate);
+	}
+	
+	@Test(expected = DateTimeException.class)
+	public void 유효하지_않은_날짜를_만든다면() {
+		LocalDate localDate = LocalDate.of(2019, 2, 32);				
+	}
+
 }
